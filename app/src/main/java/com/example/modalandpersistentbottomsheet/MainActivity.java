a4b66658577adec46048631f7dd9ac3f73946b7f
DevStar999 <wolverinerage99@gmail.com>
2021-12-04 14:53:56 +0530

    Adding the start content of the project

---
+++ app/src/main/java/com/example/modalandpersistentbottomsheet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.modalandpersistentbottomsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
