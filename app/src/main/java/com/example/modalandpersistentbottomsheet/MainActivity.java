ae0edfc71fd9b283c9454097729720ec7ba50568
DevStar999 <wolverinerage99@gmail.com>
2021-12-04 15:10:03 +0530

    Added separate activities for the two type of bottom sheets

--- app/src/main/java/com/example/modalandpersistentbottomsheet/MainActivity.java
+++ app/src/main/java/com/example/modalandpersistentbottomsheet/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.modalandpersistentbottomsheet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button gotoModalSheetButton;
+    private Button gotoPersistentSheetButton;
+
+    private void initialise() {
+        gotoModalSheetButton = findViewById(R.id.modal_button);
+        gotoPersistentSheetButton = findViewById(R.id.persistent_button);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialise();
+
+        gotoModalSheetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ModalSheetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gotoPersistentSheetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PersistentSheetActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/modalandpersistentbottomsheet/ModalSheetActivity.java
@@ -0,0 +1,14 @@
+package com.example.modalandpersistentbottomsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModalSheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modal_sheet);
+    }
+}
