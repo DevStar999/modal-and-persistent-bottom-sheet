b0bbce57e416206ae16db6f056f78b8761f7eaf3
DevStar999 <wolverinerage99@gmail.com>
2021-12-04 17:04:10 +0530

    Completed Implementation of Persistent Bottom Sheet

--- app/src/main/java/com/example/modalandpersistentbottomsheet/PersistentSheetActivity.java
+++ app/src/main/java/com/example/modalandpersistentbottomsheet/PersistentSheetActivity.java
@@ -1,14 +1,99 @@
 package com.example.modalandpersistentbottomsheet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class PersistentSheetActivity extends AppCompatActivity {
+    private View bottomSheet;
+    private TextView statusTextView;
+    private Button expandButton;
+    private Button collapseButton;
+
+    private BottomSheetBehavior bottomSheetBehavior;
+
+    private void initialise() {
+        bottomSheet = findViewById(R.id.persistent_bottom_sheet);
+        statusTextView = findViewById(R.id.persistent_bottom_sheet_status_text_view);
+        expandButton = findViewById(R.id.expand_button);
+        collapseButton = findViewById(R.id.collapse_button);
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_persistent_sheet);
+
+        initialise();
+
+        /* Notes related to Persistent Bottom Sheet are as follows :
+        1) The Persistent Bottom Sheet only works in a Coordinator layout, so we need to change the root layout
+        from the default Constraint layout to Coordinator layout.
+        2) The Coordinator layout is just a container which helps it's child views to move relative to each other.
+        */
+
+        expandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        collapseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        statusTextView.setText("Status: Collapsed");
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_DRAGGING: {
+                        // Dragging is only called when we adjust the bottom sheet manually with our hands or mouse
+                        statusTextView.setText("Status: Dragging...");
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        statusTextView.setText("Status: Expanded");
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_HIDDEN: {
+                        statusTextView.setText("Status: Hidden");
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_SETTLING: {
+                        // This is called at the exact moment when we leave the bottom sheet and it has to snap either to
+                        // an expanded state or collapsed state
+                        statusTextView.setText("Status: Settling...");
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_HALF_EXPANDED: {
+                        statusTextView.setText("Status: Half-Expanded");
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                // This method is called when the method is sliding, so when adjust the bottom sheet using buttons,
+                // this method will be called.
+                statusTextView.setText("Status: Sliding...");
+            }
+        });
     }
 }
